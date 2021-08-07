46becdfdcb363bcb155985e1c4a9802e0698547d
Altheo Saquilayan <201801106@iacademy.edu.ph>
2021-08-08 00:24:46 +0800

    Finalizing exam

--- javascript/numberTwo.js
+++ javascript/numberTwo.js
@@ -1,16 +1,62 @@
 //The link is not working so I copied the .json and place it inside this folder.
 
 
-
 const formComponent = () => {
-    this.$form = $([
-        "<div>",
-        "  <div class='js-alert-box'></div>",
-        "  <form id='my-form-to-validate'>",
-        "    <input id='login-username' name='login-username'>",
-        "  </form>",
-        "</div>"
-    ].join("\n"));
+    const genderRadioComponent = (genderValue)=>{
+        console.log("CALLED THE RADIO COMPONENT" + genderValue)
+        if (genderValue == true){ //isMale
+            $(            `
+            <p>Please select your gender:</p>
+            </br></br>
+            <input type="radio" checked name="gender" value="Male">
+            <label for="male" >Male</label><br>
+            <input type="radio" name="gender" value="Female">
+            <label for="female">Female</label><br>
+            `).appendTo(".radioboxgender")
+        }else { //Not male
+            $(`
+            <p>Please select your gender:</p>
+            </br></br>
+            <input type="radio" name="gender" value="Male">
+            <label for="male" checked >Male</label><br>
+            <input type="radio" name="gender" value="Female">
+            <label for="female">Female</label><br>
+            `).appendTo(".radioboxgender")
+        }
+    }
+
+    const componentLanguages = (languages)=>{
+        languages.forEach(element => {
+            $(`
+            <input type="checkbox" checked id="${element}" name="${element}" value="${element}">
+            <label for="lang${element}"> ${element} </label><br>
+            `).appendTo(".checkboxlanguage");
+        });
+    }
+
+    $.getJSON("exam.json", (data_json) =>{
+        console.log(data_json);
+        //CAll child components
+        genderRadioComponent(data_json["employees"][0]["gender"]["male"])
+        componentLanguages(data_json["employees"][0]["knownLanguage"])
+        $(
+        `
+        <form method='GET'>
+            First name: 
+            <input type="text" value="${data_json["employees"][0]["firstName"]}"/>
+            Last name: 
+            <input type="text" value="${data_json["employees"][0]["lastName"]}"/>
+
+            Birthday:
+            <input type="date" value="${data_json["employees"][0]["birthday"]}"/>
+                
+            <button type="submit">
+        </form>
+        
+        `).appendTo(".formComponent");
+        
+    });
+    
+
 }
 
-export default formComponent;
--- javascript/numberTwo.php
+++ javascript/numberTwo.php
@@ -4,39 +4,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <script src="./numberTwo.js"></script>
+    <script>
+        formComponent();
+    </script>
     <title>Javascript - 2</title>
 
 <body>
     <img src="../images/iacademy.png">
     <h4>Javascript GET</h4>
     <hr />
-    <?php ?>
+    <span class="formComponent">
+    </span>
+    <span class="radioboxgender"> </span>
+    <p>Please select your language</p>
+    </br></br>
+
+    <span class="checkboxlanguage"> </span>
     <form method="GET">
-        First name: 
-        <input type="text">
-        Last name: 
-        <input type="text">
-        Birthday:
-        <input type="date">
-        <p>Please select your gender:</p>
-        </br></br>
 
-        <input type="radio" name="gender" value="Male">
-        <label for="male">Male</label><br>
-        <input type="radio" name="gender" value="Female">
-        <label for="female">Female</label><br>
-
-        <p>Please select your language</p>
-        </br></br>
-        <input type="checkbox" id="lang1" name="lang1" value="English">
-        <label for="lang1"> English </label><br>
-        <input type="checkbox" id="lang2" name="lang2" value="Japanese">
-        <label for="lang2"> Japanese </label><br>
-        <input type="checkbox" id="lang3" name="lang3" value="Chinese">
-        <label for="lang3"> Chinese </label><br>
-    </form>
 
 </body>
 </html> 
